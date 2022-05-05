2e5275a020ba34b1415508c7e0ea4d6fb87d2967
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2022-05-05 16:05:21 -0500

    woops

--- src/main/java/com/unrealdinnerbone/prigadier/PrigadierHelpers.java
+++ src/main/java/com/unrealdinnerbone/prigadier/PrigadierHelpers.java
@@ -1,2 +1,11 @@
-package com.unrealdinnerbone.prigadier;public class PrigadierHelpers {
+package com.unrealdinnerbone.prigadier;
+
+import com.destroystokyo.paper.brigadier.BukkitBrigadierCommandSource;
+import org.bukkit.entity.Player;
+
+public class PrigadierHelpers
+{
+    public static boolean isPlayer(BukkitBrigadierCommandSource s) {
+        return s.getBukkitSender() instanceof Player;
+    }
 }
