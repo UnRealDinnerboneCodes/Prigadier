cd42ca522362830c956f23994602660e9d544727
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2023-07-07 13:23:36 -0500

    Messages

---
+++ src/main/java/com/unrealdinnerbone/prigadier/api/Messages.java
@@ -0,0 +1,18 @@
+package com.unrealdinnerbone.prigadier.api;
+
+import com.destroystokyo.paper.brigadier.BukkitBrigadierCommandSource;
+import io.papermc.paper.adventure.PaperAdventure;
+import net.minecraft.commands.CommandSourceStack;
+
+import java.util.function.Supplier;
+
+public class Messages
+{
+    public void sendSuccess(BukkitBrigadierCommandSource source, Supplier<net.kyori.adventure.text.Component> feedbackSupplier, boolean broadcastToOps) throws IllegalArgumentException {
+        if(source instanceof CommandSourceStack stack) {
+            stack.sendSuccess(() -> PaperAdventure.asVanilla(feedbackSupplier.get()), broadcastToOps);
+        }else {
+            throw new IllegalArgumentException("Source must be a CommandSourceStack");
+        }
+    }
+}
