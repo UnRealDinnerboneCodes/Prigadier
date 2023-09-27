5bcbd9fe74ee594206cd2b35011ee51ac5f9c329
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2023-09-27 17:09:09 -0500

    Player Assert

--- src/main/java/com/unrealdinnerbone/prigadier/api/Exceptions.java
+++ src/main/java/com/unrealdinnerbone/prigadier/api/Exceptions.java
@@ -1,13 +1,22 @@
 package com.unrealdinnerbone.prigadier.api;
 
+import com.destroystokyo.paper.brigadier.BukkitBrigadierCommandSource;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.BuiltInExceptionProvider;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.commands.CommandSourceStack;
 
 public class Exceptions {
-        public static final BuiltInExceptionProvider BUILT_IN_EXCEPTIONS = CommandSyntaxException.BUILT_IN_EXCEPTIONS;
-        public static final SimpleCommandExceptionType ERROR_NOT_PLAYER = CommandSourceStack.ERROR_NOT_PLAYER;
-        public static final SimpleCommandExceptionType ERROR_NOT_ENTITY = CommandSourceStack.ERROR_NOT_ENTITY;
+    public static final BuiltInExceptionProvider BUILT_IN_EXCEPTIONS = CommandSyntaxException.BUILT_IN_EXCEPTIONS;
+    public static final SimpleCommandExceptionType ERROR_NOT_PLAYER = CommandSourceStack.ERROR_NOT_PLAYER;
+    public static final SimpleCommandExceptionType ERROR_NOT_ENTITY = CommandSourceStack.ERROR_NOT_ENTITY;
+
+
+    public void assertPlayer(CommandContext<BukkitBrigadierCommandSource> context) throws CommandSyntaxException {
+        if(context.getSource().getBukkitSender() == null) {
+            throw ERROR_NOT_PLAYER.create();
+        }
     }
+}
     
